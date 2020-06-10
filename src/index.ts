66d98037217df633902983ecf597ca0ca7149d87
Chloe Strainge <chloe.strainge@wegalvanize.com>
2020-06-10 00:04:40 -0700

    Simple server running now.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 
-console.log("Hello world.");
+import * as path from 'path';
+import express from 'express';
+
+const app: express.Application = express();
+
+
+app.get('/', (request: express.Request, response: express.Response) => {
+    const indexPath = path.join(__dirname, '..', "index.html");
+    console.log(`Sending ${indexPath}.`);
+
+    response.sendFile(indexPath);
+});
+
+app.listen(8008);
